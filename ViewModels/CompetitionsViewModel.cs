53557dd26b65af5c994e332c490fcb56ea68cf2d
flown4qqqq <igorkoshkarev4qqq@mail.ru>
2024-09-27 01:23:10 +0300

    now main window and competitions are working

--- ViewModels/CompetitionsViewModel.cs
+++ ViewModels/CompetitionsViewModel.cs
@@ -11,39 +11,6 @@ public class CompetitionsViewModel : SubViewModel
 	public CompetitionsViewModel()
 	{   
         var x = QueryComposer.JoinCompetitions();
-
-        System.Console.WriteLine("GOO");
-        foreach (var competition in x) {
-            System.Console.WriteLine(competition.Name + " " + competition.Date + " " + competition.NameOfSport + " " + competition.Place);
-        }
-
 		Competitions = new ObservableCollection<Competition>(QueryComposer.JoinCompetitions());
 	}
-
-	// public void AddDoctor()
-	// {
-	// 	NewDoctorViewModel newDoctor = new NewDoctorViewModel(App.DB.GetNextId("tblDoctor"), Doctors.Count);
-
-	// 	Observable.Merge(
-	// 		newDoctor.Add,
-	// 		newDoctor.Back.Select(_ => (Doctor?)null))
-	// 		.Take(1)
-	// 		.Subscribe(newItem =>
-	// 		{
-	// 			if (newItem != null)
-	// 			{
-	// 				Doctors.Add(newItem);
-	// 				App.DB.AddDoctor(newItem);
-	// 			}
-
-	// 			App.TopWindow.RemoveTopView();
-	// 		});
-
-	// 	App.TopWindow.AddView(newDoctor);
-	// }
-
-	// public void OpenDoctorReceptions(int idx)
-	// {
-	// 	App.TopWindow.AddView(new DoctorReceptionsViewModel(Doctors[idx]));
-	// }
 }
